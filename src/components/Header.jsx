fba659d2d332c9a3113984fde312afbf1b6f6339
eustachekamala <eustachejared25@gmail.com>
2024-09-06 12:02:08 +0300

    I added the logout page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,10 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const login = () => {
-    setIsLoggedIn(true);
-  };
-
-  const logout = () => {
-    setIsLoggedIn(false);
-  };
-
   return (
     <header className='flex justify-between items-center py-4 px-4 bg-slate-100'>
       <h1 className='text-2xl font-bold'>Expense Tracker</h1>
-      {isLoggedIn ? (
-        <button onClick={logout}  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'> <Link to='/logout'>Logout</Link></button>
-      ) : (
-        <button onClick={login} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'> <Link to='/'>Login</Link></button>
-      )}
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'> <Link to='/logout'>Logout</Link></button>
     </header>
   );
 }
